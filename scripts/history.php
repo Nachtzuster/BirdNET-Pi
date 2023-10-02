b7548d4a2bcaaa19f69c2585d628349f3e29ac02
Louis Croisez <louis.croisez@gmail.com>
2023-10-02 16:05:47 +0200

    Added swipeL + swipeR + arrowL + arrowR events to DailyCharts form

--- scripts/history.php
+++ scripts/history.php
@@ -144,6 +144,8 @@ function submitID() {
   <input type="date" name="date" value="<?php echo $theDate;?>">
   <button type="submit" name="view" value="Daily Charts">Submit Date</button>
 </form>
+<div id="divInfoSwipe" style="text-align:center;">
+</div>
 		<table>
 			<tr>
 				<th>Total Detections For The Day</th>
