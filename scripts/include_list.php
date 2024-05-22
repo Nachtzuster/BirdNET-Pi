1b253cf6eef79621b027a10c35e3a8e910db8b66
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-05-22 09:50:54 +0200

    Re-add dummy "Please select"option

--- scripts/include_list.php
+++ scripts/include_list.php
@@ -21,6 +21,7 @@ $eachlines = file($filename, FILE_IGNORE_NEW_LINES);
   <h2>All Species Labels</h2>
   <input autocomplete="off" size="18" type="text" placeholder="Search Species..." id="species_searchterm" name="species_searchterm">
   <select name="species[]" id="species" multiple size="25">
+  <option selected value="base">Please Select</option>
       <?php   
         foreach($eachlines as $lines){echo 
     "<option value=\"".$lines."\">$lines</option>";}
@@ -45,6 +46,7 @@ $eachlines = file($filename, FILE_IGNORE_NEW_LINES);
 <form action="" method="GET" id="del">
   <h2>Custom Species List</h2>
   <select name="species[]" id="value2" multiple size="25">
+  <option selected value="base">Please Select</option>
       <?php
         $filename = './scripts/include_species_list.txt';
         $eachlines = file($filename, FILE_IGNORE_NEW_LINES);
