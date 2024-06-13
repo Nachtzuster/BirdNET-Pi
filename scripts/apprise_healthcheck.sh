87cd34258c2e1eef79846234e5ea3b3be58983d3
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-06-13 08:45:14 +0200

    Update apprise_healthcheck.sh

--- scripts/apprise_healthcheck.sh
+++ scripts/apprise_healthcheck.sh
@@ -1,29 +1,31 @@
 #!/usr/bin/env bash
 source /etc/birdnet/birdnet.conf
 
-if [ -s "$HOME/BirdNET-Pi/apprise.txt" ] && [ ${APPRISE_HOURLY_HEALTHCHECK} == 1 ]; then
-    if [[ "$(sudo systemctl is-active birdnet_analysis.service)" == "active" ]]; then
-            export LASTCHECK="$(date +%c)"
-    else
-    	    NOTIFICATION="System : $(hostname)"
-            NOTIFICATION+="<br>Last successful check : ${LASTCHECK:-never})"
-            STOPPEDSERVICE="<b>Stopped services:</b> "
-            services=(birdnet_analysis
-	    	chart_viewer
-                spectrogram_viewer
-                icecast2
-                birdnet_recording
-                birdnet_log
-                birdnet_stats)
-            for i in  "${services[@]}"; do
-                if [[ "$(sudo systemctl is-active "${i}".service)" == "inactive" ]]; then
-                    STOPPEDSERVICE+="${i}; "
-                fi
-            done
-#        if [ -n "$BIRDNETPI_URL" ]; then
-#            NOTIFICATION+="<br> <a href=$BIRDNETPI_URL>Access your BirdNET-Pi instance</a>"
-#        fi
+# Check if the analysis and recording services are running, or alert the user if apprise is configured
 
+if [ -s "$HOME/BirdNET-Pi/apprise.txt" ] && [ ${APPRISE_HOURLY_HEALTHCHECK} == 1 ]; then
+    if [[ "$(sudo systemctl is-active birdnet_analysis.service)" == "active" ]] && [[ "$(sudo systemctl is-active birdnet_recording.service)" == "active" ]]; then
+        export LASTCHECK="$(date +%c)"
+    else
+        NOTIFICATION="<br><b>Since:</b> ${LASTCHECK:-unknown}"
+        NOTIFICATION="<br><b>System:</b> ${SITE_NAME:-$(hostname)}"
+        STOPPEDSERVICE="<br><b>Stopped services:</b> "
+        services=(birdnet_analysis
+            chart_viewer
+            spectrogram_viewer
+            icecast2
+            birdnet_recording
+            birdnet_log
+            birdnet_stats)
+        for i in "${services[@]}"; do
+            if [[ "$(sudo systemctl is-active "${i}".service)" == "inactive" ]]; then
+                STOPPEDSERVICE+="${i}; "
+            fi
+        done
+        NOTIFICATION+="$STOPPEDSERVICE"
+        if [ -n "$BIRDNETPI_URL" ]; then
+            NOTIFICATION+="<br> <a href=\"$BIRDNETPI_URL\">Access your BirdNET-Pi instance</a>"
+        fi
         TITLE="BirdNET-Analyzer stopped"
         $HOME/BirdNET-Pi/birdnet/bin/apprise -vv -t "$TITLE" -b "${NOTIFICATION}" --input-format=html --config="$HOME/BirdNET-Pi/apprise.txt"
     fi
