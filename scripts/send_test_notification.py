e7be308dcdc156f234fc18ca520ec922d6ca2495
frederik <frederik.slos@gmail.com>
2025-10-19 11:22:09 +0200

    fix empty db

--- scripts/send_test_notification.py
+++ scripts/send_test_notification.py
@@ -1,4 +1,5 @@
 import argparse
+import datetime
 import json
 import logging
 import sys
@@ -23,15 +24,22 @@ if __name__ == "__main__":
     notifications.APPRISE_CONFIG = args.config
     notifications.APPRISE_BODY = args.body
 
-    f = open(args.detection, 'r')
-    d = json.loads(f.read())
-
     logger = logging.getLogger()
     formatter = logging.Formatter("[%(name)s][%(levelname)s] %(message)s")
     handler = logging.StreamHandler(stream=sys.stdout)
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     logger.setLevel(logging.INFO)
+
+    f = open(args.detection, 'r')
+    try:
+        d = json.loads(f.read())
+    except json.JSONDecodeError:
+        now = datetime.datetime.now()
+        d = {'Sci_Name': 'Aptenodytes patagonicus', 'Com_Name': 'King Penguin', 'Confidence': 0.84, 'File_Name': 'this_is_not_a_file.mp3',
+             'Date': now.strftime('%Y-%m-%d'), 'Time': now.strftime("%H:%M:%S"), 'Week': now.isocalendar()[1],
+             'Lat': conf.getfloat('LATITUDE'), 'Lon': conf.getfloat('LONGITUDE'), 'Cutoff': conf.getfloat('CONFIDENCE'),
+             'Sens': conf.getfloat('SENSITIVITY'), 'Overlap': conf.getfloat('OVERLAP')}
 
     notifications.sendAppriseNotifications(f"{d['Sci_Name']}_{d['Com_Name']}", d['Confidence'], round(d['Confidence'] * 100), d['File_Name'],
                                            d['Date'], d['Time'], d['Week'], d['Lat'], d['Lon'], d['Cutoff'],
